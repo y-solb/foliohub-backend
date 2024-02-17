8584315d0bcc3dea998d60f74b0dcb84c1d8d547
y-solb <ysolb99@gmail.com>
2024-02-18 01:57:02 +0900

    Feat: PostgreSQL, typeorm, docker 설정추가

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
 import cors from "cors";
+import { AppDataSource } from "./data-source";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app: Express = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use("/", routes);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
-});
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at https://localhost:${port}`);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
