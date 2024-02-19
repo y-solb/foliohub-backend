e51e41a5cb6568831451114daeeb8a00f7140fd4
y-solb <ysolb99@gmail.com>
2024-02-20 02:00:32 +0900

    Feat: token 관련 함수 추가

---
+++ src/libs/token.ts
@@ -0,0 +1,31 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { Response } from "express";
+
+export const generateToken = (payload: any, options?: SignOptions) => {
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY is not defined");
+  }
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, options);
+};
+
+export const decodeToken = (token: string) => {
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY is not defined");
+  }
+  return jwt.verify(token, process.env.JWT_SECRET_KEY);
+};
+
+export function setTokenCookie(res: Response, refreshToken: string) {
+  // const { accessToken, refreshToken } = tokens;
+
+  // res.cookie("accessToken", accessToken, {
+  //   // maxAge: 1000 * 60 * 60,
+  //   maxAge: 60 * 1000,
+  //   httpOnly: true,
+  // });
+
+  res.cookie("refreshToken", refreshToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+    httpOnly: true,
+  });
+}
