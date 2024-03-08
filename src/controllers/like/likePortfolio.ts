b60274b94ab3942d6acfc76815467bf24ebf8d54
y-solb <ysolb99@gmail.com>
2024-03-08 13:01:09 +0900

    Feat: 좋아요에 따라 likeCount 증가 및 감소되도록 추가

--- src/controllers/like/likePortfolio.ts
+++ src/controllers/like/likePortfolio.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../../libs/customError';
 import { AppDataSource } from '../../data-source';
 import { LikePortfolio } from '../../entities/LikePortfolio';
+import { Portfolio } from '../../entities/Portfolio';
 
 export const likePortfolio = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.user;
@@ -20,25 +21,33 @@ export const likePortfolio = async (req: Request, res: Response, next: NextFunct
     });
     if (existingLike) {
       existingLike.status = true;
+
       await likePortFolioRepository.save(existingLike);
-      return res.json({
-        success: true,
-        message: '좋아요가 성공적으로 업데이트되었습니다.',
-        isLike: true,
-      });
+    } else {
+      const newLike = new LikePortfolio();
+      newLike.fkPortfolioId = portfolioId;
+      newLike.fkUserId = id;
+      newLike.status = true;
+
+      await likePortFolioRepository.save(newLike);
     }
 
-    const newLike = new LikePortfolio();
-    newLike.fkPortfolioId = portfolioId;
-    newLike.fkUserId = id;
-    newLike.status = true;
+    const portFolioRepository = AppDataSource.getRepository(Portfolio);
+    const portfolio = await portFolioRepository.findOne({
+      where: { id: portfolioId },
+    });
 
-    await likePortFolioRepository.save(newLike);
+    if (!portfolio) {
+      return next(new CustomError(400, 'Validation', '해당 portfolioId값이 존재하지 않습니다.'));
+    }
+    portfolio.likeCount = portfolio.likeCount + 1;
+    await portFolioRepository.save(portfolio);
 
     return res.json({
       success: true,
       message: '좋아요가 성공적으로 업데이트되었습니다.',
       isLike: true,
+      likeCount: portfolio.likeCount,
     });
   } catch (error) {
     return next(new CustomError(400, 'Raw', 'Error', null, error));
@@ -60,16 +69,30 @@ export const unlikePortfolio = async (req: Request, res: Response, next: NextFun
     const existingLike = await likePortFolioRepository.findOne({
       where: { fkPortfolioId: portfolioId, fkUserId: id },
     });
-    if (existingLike) {
-      existingLike.status = false;
-      await likePortFolioRepository.save(existingLike);
-      return res.json({
-        success: true,
-        message: '좋아요가 성공적으로 업데이트되었습니다.',
-        isLike: false,
-      });
+    if (!existingLike) {
+      return next(new CustomError(400, 'Validation', '해당 값이 존재하지 않습니다.'));
     }
-    return next(new CustomError(400, 'Validation', '해당 값이 존재하지 않습니다.'));
+
+    existingLike.status = false;
+    await likePortFolioRepository.save(existingLike);
+
+    const portFolioRepository = AppDataSource.getRepository(Portfolio);
+    const portfolio = await portFolioRepository.findOne({
+      where: { id: portfolioId },
+    });
+
+    if (!portfolio) {
+      return next(new CustomError(400, 'Validation', '해당 portfolioId값이 존재하지 않습니다.'));
+    }
+    portfolio.likeCount = portfolio.likeCount - 1;
+    await portFolioRepository.save(portfolio);
+
+    return res.json({
+      success: true,
+      message: '좋아요가 성공적으로 업데이트되었습니다.',
+      isLike: false,
+      likeCount: portfolio.likeCount,
+    });
   } catch (error) {
     return next(new CustomError(400, 'Raw', 'Error', null, error));
   }
