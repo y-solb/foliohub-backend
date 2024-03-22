c8b5b26b16a1ecafaba97ac17df6fcaa242ee947
y-solb <ysolb99@gmail.com>
2024-03-22 14:24:08 +0900

    Fix: host 수정

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
