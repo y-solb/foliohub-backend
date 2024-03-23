e971ccea4a0e755724a82478ccf97091fc917210
y-solb <ysolb99@gmail.com>
2024-03-24 00:19:41 +0900

    Fix

--- dist/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (error, req, res) => {
-    return res.status(error.HttpStatusCode).json(error.JSON);
-};
-exports.default = errorHandler;
