27a1f969110e92961b95e374023940b80032bce2
y-solb <ysolb99@gmail.com>
2024-03-31 11:32:38 +0900

    Chore: build

--- dist/data-source.js
+++ dist/data-source.js
@@ -17,10 +17,9 @@ exports.AppDataSource = new typeorm_1.DataSource({
     database: process.env.POSTGRES_DB,
     synchronize: true,
     logging: true,
-    // entities: [
-    //   process.env.NODE_ENV === 'development' ? 'src/entities/**/*.ts' : 'src/entities/**/*.ts',
-    // ],
-    entities: ['dist/entities/**/*.js'],
+    entities: [
+        process.env.NODE_ENV === 'development' ? 'src/entities/**/*.ts' : 'dist/entities/**/*.js',
+    ],
     subscribers: [],
     migrations: [],
 });
