d6574770c965fbd229e4330a96a5b2e52eb5d469
y-solb <ysolb99@gmail.com>
2024-04-06 01:13:31 +0900

    Fix: 에러 json 형태로 나오도록 수정

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../libs/customError';
 
-const errorHandler = (error: CustomError, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorHandler = (error: CustomError, req: Request, res: Response, next: NextFunction) => {
   return res.status(error.HttpStatusCode).json(error.JSON);
 };
 
