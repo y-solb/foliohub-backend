d91912d2708603f225c955617621ed7867cfbb8c
y-solb <ysolb99@gmail.com>
2024-04-06 01:15:56 +0900

    Chore: build

--- dist/middlewares/errorHandler.js
+++ dist/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (error, req, res) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorHandler = (error, req, res, next) => {
     return res.status(error.HttpStatusCode).json(error.JSON);
 };
 exports.default = errorHandler;
