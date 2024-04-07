ea10004015138fe0cd6094a821469fe9143ba315
y-solb <ysolb99@gmail.com>
2024-04-07 23:27:59 +0900

    Chore: 데이터 추가용

--- src/controllers/auth/loginExperience.ts
+++ src/controllers/auth/loginExperience.ts
@@ -10,15 +10,16 @@ import { CustomError } from '../../libs/customError';
  */
 export const loginExperience = async (req: Request, res: Response, next: NextFunction) => {
   const { code } = req.body;
-  if (!code || code !== 'HelloWorld') {
-    return next(new CustomError(400, 'General', '타당하지 않은 code입니다.'));
-  }
+  // if (!code || code !== 'HelloWorld') {
+  //   return next(new CustomError(400, 'General', '타당하지 않은 code입니다.'));
+  // }
 
   try {
     const userRepository = AppDataSource.getRepository(User);
     const user = await userRepository.findOne({
       where: {
-        providerId: 'test',
+        // providerId: 'test',
+        username: code,
       },
     });
 
