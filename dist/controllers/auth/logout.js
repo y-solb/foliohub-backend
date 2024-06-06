009f628c59f93caafaa16029cea77d4f227f3da7
y-solb <ysolb99@gmail.com>
2024-06-06 18:43:48 +0900

    Chore: build

--- dist/controllers/auth/logout.js
+++ dist/controllers/auth/logout.js
@@ -8,16 +8,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logout = void 0;
 const customError_1 = require("../../libs/customError");
+const data_source_1 = require("../../data-source");
+const AuthToken_1 = __importDefault(require("../../entities/AuthToken"));
 /**
  * 로그아웃
  * POST /v1/auth/logout
  */
 const logout = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // TODO: AuthToken에서 삭제해야 하는지
+        const { refreshToken } = req.cookies;
+        if (!refreshToken) {
+            return next(new customError_1.CustomError(401, 'Unauthorized', 'refreshToken이 존재하지 않습니다.'));
+        }
+        const authTokenRepository = data_source_1.AppDataSource.getRepository(AuthToken_1.default);
+        yield authTokenRepository.delete({
+            token: refreshToken,
+        });
         res.clearCookie('accessToken', { domain: process.env.COOKIE_DOMAIN, path: '/' });
         res.clearCookie('refreshToken', { domain: process.env.COOKIE_DOMAIN, path: '/' });
         res.json({ message: '로그아웃 처리 되었습니다.' });
