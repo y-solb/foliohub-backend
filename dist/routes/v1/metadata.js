eb3bf037497b52505357cd92ee4add4ed3b35501
y-solb <ysolb99@gmail.com>
2024-06-09 13:25:39 +0900

    Remove: 빌드 소스 제거

--- dist/routes/v1/metadata.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./../../controllers/metadata/index");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', index_1.extractUrlMetadata);
-exports.default = router;
