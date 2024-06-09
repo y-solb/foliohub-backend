837a9c3dd69fa085eefbb40147bc7f2f88939906
y-solb <ysolb99@gmail.com>
2024-06-09 11:22:30 +0900

    Remove: 빌드 소스 제거

--- dist/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const errorHandler = (error, req, res, next) => {
-    return res.status(error.HttpStatusCode).json(error.JSON);
-};
-exports.default = errorHandler;
