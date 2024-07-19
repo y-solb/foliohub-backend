3a1118f0d3f3c7f8b379db8be761546b23ce8f43
y-solb <ysolb99@gmail.com>
2024-07-19 22:19:26 +0900

    Chore: test용 메시지 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import v1 from './v1/';
 
 const router = Router();
 
-router.get('/', (_, res) => res.send("it's running 👍"));
+router.get('/', (_, res) => res.send("it's running 👍👍"));
 router.use('/v1', v1);
 
 export default router;
